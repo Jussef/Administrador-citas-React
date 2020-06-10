e9052da49971d260f3cf69168c5e419d649bf6cc
Jussefo91 <jussefspira@users.noreply.github.com>
2020-06-10 10:28:18 -0500

    init

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,83 @@
+import React, { Fragment, useState } from "react";
+
+const Formulario = () => {
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  // Función que se ejecuta cada que el usuario escribe
+  const actualizarState = (e) => {
+    console.log(e.target.name);
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //   Extraer los valores para no escribir cita.nombre
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+
+      <form>
+        <label htmlFor="mascota">Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+
+        <label htmlFor="propietario">Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+
+        <label htmlFor="fecha">Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+
+        <label htmlFor="hora">Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+
+        <label htmlFor="sintomas">Sintomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
